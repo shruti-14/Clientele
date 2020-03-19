40d96f00c286c9986c9ff78543951c5c9f0cac4d
ShrutiBadekar <shrtbadekar@gmail.com>
2020-03-20 01:30:13 +0530

    login page added

--- src/main-page/main-page.component.js
+++ src/main-page/main-page.component.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 function Main() {
     return <div><ul>
     <li><Link to="/login">Login</Link></li>
-    <li><Link to="/register">Register</Link></li>
+    {/* <li><Link to="/register">Register</Link></li> */}
     </ul></div>
 }
 export default Main;
