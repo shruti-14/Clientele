d0c4afdcf556478caa529ecf6b5e44a14ad0bc71
ShrutiBadekar <shrtbadekar@gmail.com>
2020-06-15 00:06:28 +0530

    positioning of pop-up component half done

--- src/commons/Navigation/Toolbar/Toolbar.js
+++ src/commons/Navigation/Toolbar/Toolbar.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import classes from './Toolbar.module.css';
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 import IconNavigationItems from '../IconNavigationItems/IconNavigationItems';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Aux from '../../../hoc/Aux/Aux';
 
-const toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <DrawerToggle clicked={props.drawerToggleClicked} />
-        <div className={classes.LeftOptionContainer}>
-            <p className={classes.Title}>
-                Clientele
+const Toolbar = (props) => {
+    const navLinksRef = useRef(null);
+    const [popUpStyle, setPopUpStyle] = useState({
+        position: 'relative',
+        left: 0,
+        top: '80px'
+    });
+    const getStyles = () => {
+        let style = {
+            position: 'relative',
+            left: 0,
+            top: '80px'
+        }
+        debugger;
+        if (navLinksRef.current) {
+
+            const elePosition = navLinksRef.current.getBoundingClientRect();
+            style['left'] = elePosition['left'] + 'px';
+            style['top'] = elePosition['top'] + 'px';
+        }
+        console.log(style);
+        setPopUpStyle(style)
+    }
+    return (<Aux>
+        <header className={classes.Toolbar}>
+            <DrawerToggle clicked={props.drawerToggleClicked} />
+            <div className={classes.LeftOptionContainer}>
+                <p className={classes.Title}>
+                    Clientele
              </p>
-            <div className={classes.mainNavLinks}> <NavigationItems /> </div>
+                <div className={classes.mainNavLinks} ref={navLinksRef}> <NavigationItems /> </div>
 
+            </div>
+            <IconNavigationItems />
+        </header>
+        <div className={classes.CategoryContainer} style={popUpStyle} onClick={() => getStyles()}>
+            abc
         </div>
-        <IconNavigationItems />
-    </header>
-);
+    </Aux>);
+};
 
-export default toolbar;
+export default Toolbar;
