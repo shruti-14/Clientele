f5a93dff5bf2886b4d0559846e0d0fcb9fb057b6
ShrutiBadekar <shrtbadekar@gmail.com>
2020-07-18 02:10:43 +0530

    Login form desgining done

--- src/cointainer/Login/Login.js
+++ src/cointainer/Login/Login.js
@@ -34,17 +34,10 @@ class Login extends Component {
             }
         },
         formIsValid: false,
-        loading: false,
-        loginTabActive: true
+        loading: false
     }
     inputChangedHandler = (event, inputIdentifier) => {
         console.log('Hi');
-    }
-    loginTabClicked = () => {
-        this.setState({ loginTabActive: true });
-    }
-    registerTabClicked = () => {
-        this.setState({ loginTabActive: false });
     }
     render() {
         const formElementsArray = [];
@@ -55,30 +48,40 @@ class Login extends Component {
             });
         }
         let form = (
-            <form className={classes.form}>
-                {formElementsArray.map(formElement => (
-                    <Input
-                        key={formElement.id}
-                        elementType={formElement.config.elementType}
-                        elementConfig={formElement.config.elementConfig}
-                        value={formElement.config.value}
-                        invalid={!formElement.config.valid}
-                        shouldValidate={formElement.config.validation}
-                        touched={formElement.config.touched}
-                        changed={(event) => this.inputChangedHandler(event, formElement.id)}
+            <div className={classes.formContainer}>
+                <form className={classes.form}>
+                    {formElementsArray.map(formElement => (
+                        <Input
+                            key={formElement.id}
+                            elementType={formElement.config.elementType}
+                            elementConfig={formElement.config.elementConfig}
+                            value={formElement.config.value}
+                            invalid={!formElement.config.valid}
+                            shouldValidate={formElement.config.validation}
+                            touched={formElement.config.touched}
+                            changed={(event) => this.inputChangedHandler(event, formElement.id)}
 
-                    />
-                ))}
-                {/* <button to="/" className={classes.loginButton}>Login</button> */}
-            </form>
+                        />
+                    ))}
+                    {/* <button to="/" className={classes.loginButton}>Login</button> */}
+                </form>
+                <p className={classes.forgetPassword}>Forgot your password?</p>
+                <button className={classes.signInButton}>Sign in</button>
+            </div>
         )
         return <div className={classes.LoginContainer}>
             <div className={classes.HeaderSection}>
                 <h1 className={classes.signInTitle}>Sign in to Clientele</h1>
-                {/* <div className={classes.socialIconsContainer}>
-                    <i className={`fa ${classes.socialIcon}`}>&#xf002;</i>
-                    <i className={`fa ${classes.socialIcon}`}>&#xf002;</i>
-                </div> */}
+                <div className={classes.socialIconsContainer}>
+                    <i className={`fa ${classes.socialIcon} ${classes.gmailIcon}`}>&#xf0d5;</i>
+                    <i className={`fa ${classes.socialIcon} ${classes.facebookIcon}`}>&#xf09a;</i>
+                </div>
+
+            </div>
+            <div className={classes.orContainer}>
+                <div className={classes.divider}></div>
+                <p>or</p>
+                <div className={classes.divider}></div>
             </div>
             {form}
         </div>
